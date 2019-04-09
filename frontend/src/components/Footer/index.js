9d655f87389fa59690af19f8d860222f2ce3ffaa
André Broman <laykith@gmail.com>
2019-04-09 23:29:18 +0200

    Added inital footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Footer = props => {
+  
+  const {information, sections} = props
+
+  return (
+    <footer className="footer">
+      <div className="sections">
+        {sections.map((section, index) => {
+          return (
+            <div className="section" key={index}>
+              <h3 className="title">{section.title}</h3>
+                {section.links.map((item, index) => {
+                  return <a key={index} className="link" href={item.link}>{item.text}</a>    
+                })}
+            </div>
+          )
+        })}
+      </div>  
+      <div className="bottom">
+        {information.map(item => {
+          return <p>{item}</p>;
+        })}
+      </div>
+        
+    </footer>
+  );
+};
+
+Footer.propTypes = {
+  information: PropTypes.array,
+  sections: PropTypes.array,
+};
+
+export default Footer;
