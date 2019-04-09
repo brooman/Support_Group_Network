6c4cd1b2eb3d180fac9e0683812061f39e70db7f
André Broman <laykith@gmail.com>
2019-04-09 23:44:31 +0200

    Header-offset container

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -23,31 +23,34 @@ class Header extends React.Component {
     const navClasses = this.state.navigationOpen ? "navigation open" : "navigation"
 
     return (
-      <header className="header">
-        <div className="brand" href="/">
-          <img className="brandlogo" src={brand.logo} alt={brand.name}/>
-        </div>
+      <>
+        <header className="header">
+          <div className="brand" href="/">
+            <img className="brandlogo" src={brand.logo} alt={brand.name}/>
+          </div>
 
-        <div className={navClasses}>
-          <nav className="navitems">
-            {navitems.map((item, index) => {
+          <div className={navClasses}>
+            <nav className="navitems">
+              {navitems.map((item, index) => {
 
-              return (
-                <a href={item.link} className="navitem" key={index}>
-                  <span>
-                    {item.name}
-                  </span>
-                </a>
-              )
-              
-            })}
-          </nav>
-        </div>
+                return (
+                  <a href={item.link} className="navitem" key={index}>
+                    <span>
+                      {item.name}
+                    </span>
+                  </a>
+                )
+                
+              })}
+            </nav>
+          </div>
 
-        <div className="hamburger">
-          <Icon icon="menu" onClick={this.toggleMenu} />
-        </div>
-      </header>
+          <div className="hamburger">
+            <Icon icon="menu" onClick={this.toggleMenu} />
+          </div>
+        </header>
+        <div className="header-offset" />
+      </>
       );
   }
 };
