ea8a755cf4ae0d36299b0366e50369075ec6e3d0
André Broman <laykith@gmail.com>
2019-04-10 21:02:12 +0200

    Fixed React warnings

--- frontend/src/components/Hero/index.js
+++ frontend/src/components/Hero/index.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 class Hero extends Component {
   render() {
     return (
-      <div class="hero" style={{backgroundImage: `url(/static/images/hero.png)`}}>
-        <div class="text">
+      <div className="hero" style={{backgroundImage: `url(/static/images/hero.png)`}}>
+        <div className="text">
           <h1 className="heading">Support Group Network</h1>
           <h2 className="heading yellow">Working together for a better future</h2>
         </div>
