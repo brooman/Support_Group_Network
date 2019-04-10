4322d98b77b841c19083ac2065c4c0284467517b
André Broman <laykith@gmail.com>
2019-04-10 20:57:03 +0200

    Added card

---
+++ frontend/src/components/Card/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = props => {
+  const {image, title, content} = props
+
+  return (
+    <div className="card">
+      <div className="image" style={{backgroundImage: `url('${image}')`}}></div>
+      <h3><span className="heading lightdark">{title}</span></h3>
+      <div className="card-content">
+        <p className="text">{content}</p>
+      </div>
+      <a className="link" href="">Learn more...</a>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  
+};
+
+export default Card;
