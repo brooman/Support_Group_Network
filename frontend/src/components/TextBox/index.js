628f58ebea52ada44b874a00255a2079e199e171
André Broman <laykith@gmail.com>
2019-04-11 13:54:51 +0200

    Updated textbox

--- frontend/src/components/TextBox/index.js
+++ frontend/src/components/TextBox/index.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 const TextBox = props => {
   const {title, content, color, isWide, isLarge, link} = props
 
+  const classes = `${color ? color : ''} ${isWide ? 'wide' : ''} ${isLarge ? 'large' : ''}`
+
   return (
-    <div className={`textbox ${color} ${isWide ? 'wide' : ''} ${isLarge ? 'large' : ''}`}>
+    <div className={`textbox ${classes}`}>
       <h3 className="title"><span>{title}</span></h3>
       <p className="text">{content}</p>
       {link && <a className="link" href={link.url}>{link.text}</a>}
