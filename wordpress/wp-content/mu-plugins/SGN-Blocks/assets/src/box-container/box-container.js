070fc0c08b0a6fa520c2e2c697a59167e9e65d9b
Benjamin Fransson <benjamin@cafcon.se>
2019-04-16 22:40:09 +0200

    Moved some logic from react component to block

--- wordpress/wp-content/mu-plugins/SGN-Blocks/assets/src/box-container/box-container.js
+++ wordpress/wp-content/mu-plugins/SGN-Blocks/assets/src/box-container/box-container.js
@@ -15,18 +15,42 @@ registerBlockType('sgnblocks/box-container', {
     title: 'Text Block Container',
     icon: 'screenoptions',
     category: 'sgn-blocks',
-
+    
     supports: {
         align: true,
     },
-
+    
     attributes: {
         align: true,
     },
-
+    
     edit(props) {
-        const { className, setAttributes } = props;
+        const { className, children, setAttributes } = props;
         // const {  } = props.attributes;
+        
+        const getLayout = (count) => {
+            switch(count){
+                case 1:
+                return [{isLarge: true}]
+                case 2:
+                return [{isWide: true}, {isLarge: true}]
+                case 3: 
+                return [{}, {}, {isLarge: true}]
+                case 4: 
+                return [{isWide: true}, {}, {}, {isWide: true}]
+                case 5:
+                return [{}, {}, {isWide: true}, {}, {}]
+                default:
+                return [{}, {}, {}, {}, {}, {}]
+            }
+        }
+        
+        const count = React.Children.count(children)
+        const layout = getLayout(count)
+        
+        const content = React.Children.map(children, (child, index) => {
+            return React.cloneElement(child, layout[index]);
+        })
 
         return [
             <InspectorControls>
@@ -41,15 +65,16 @@ registerBlockType('sgnblocks/box-container', {
             </div>,
         ];
     },
-
+    
     save(props) {
         // const className = getBlockDefaultClassName('sgnblocks/box-container'); // For use with say, BEM
         // const {  } = props.attributes;
-
+        
         return (
             <div>
                 <InnerBlocks.Content />
             </div>
-        );
-    },
-});
+            );
+        },
+    });
+    
