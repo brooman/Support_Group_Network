e9392ef36bea0391ae959d6ee4f999debec902f8
Benjamin Fransson <benjamin@cafcon.se>
2019-04-21 00:00:34 +0200

    Implemented logic for working textbox

--- wordpress/wp-content/mu-plugins/support-group-network-blocks/src/components/textbox/index.js
+++ wordpress/wp-content/mu-plugins/support-group-network-blocks/src/components/textbox/index.js
@@ -1,29 +1,91 @@
-import { InnerBlocks } from '@wordpress/editor';
-
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
+const { RichText, InspectorControls } = wp.editor;
 
 registerBlockType( 'cgb/textbox', {
 
-	title: __( 'textbox - CGB Block' ),
+	title: __( 'Colored Textbox' ),
 	icon: 'shield',
 	category: 'sgn-blocks',
 
-	edit: function( props ) {
-		const { title, content, color, isWide, isLarge, link } = props;
+	attributes: {
+		title: {
+			type: 'string',
+			source: 'text',
+			selector: 'h2.title > span',
+		},
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: '.text',
+		},
+	},
 
-		const classes = `${ color ? color : '' } ${ isWide ? 'wide' : '' } ${ isLarge ? 'large' : '' }`;
+	edit( props ) {
+		const {
+			attributes: { title, content },
+			className,
+			setAttributes,
+		} = props;
+
+		const onChangeContent = content => {
+			setAttributes( { content } );
+		};
+
+		const onChangeTitle = title => {
+			setAttributes( { title } );
+		};
+
+		// Get value from select > option
+		const onChangeColorOption = value => {
+			// Find some way to update the styling
+			// Maybe change class, maybe something else
+		};
+
+		return [
+			<InspectorControls>
+				Options
+			</InspectorControls>,
+			<div className={ className }>
+				<h2 className="title">
+					<RichText
+						tagName="span"
+						placeholder="Add your own Title"
+						value={ title }
+						onChange={ onChangeTitle }
+					/>
+				</h2>
+
+				<RichText
+					tagName="p"
+					className="text"
+					placeholder="Add your own content"
+					value={ content }
+					onChange={ onChangeContent }
+				/>
+			</div>,
+		];
+	},
+
+	save( props ) {
+		const {
+			attributes: {
+				title,
+				content,
+			},
+		} = props;
 
 		return (
-			<div className={ `textbox ${ classes }` }>
-				<h3 className="title"><span>{ title }</span></h3>
+			<div>
+				<h2 className="title">
+					<span>
+						{ title }
+					</span>
+				</h2>
+
 				<p className="text">{ content }</p>
-				{ link && <a className="link" href={ link.url }>{ link.text }</a> }
 			</div>
 		);
 	},
-
-	save: function( ) {
-		return null;
-	},
 } );
+
