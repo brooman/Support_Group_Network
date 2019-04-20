63de33240fa6a0a15f9866d77db7d6db8f1db1a4
Benjamin Fransson <benjamin@cafcon.se>
2019-04-21 00:01:16 +0200

    Implemented support for MediaUpload & InpectCtrls

--- wordpress/wp-content/mu-plugins/support-group-network-blocks/src/components/card/index.js
+++ wordpress/wp-content/mu-plugins/support-group-network-blocks/src/components/card/index.js
@@ -1,27 +1,149 @@
+/* eslint-disable react/jsx-key */
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
+const { RichText, InspectorControls, MediaUpload } = wp.editor;
 
 registerBlockType( 'cgb/card', {
 	title: __( 'Card' ),
 	icon: 'shield',
-	category: 'common',
+	category: 'sgn-blocks',
+
+	attributes: {
+		image: {
+			type: 'string',
+			default: 'http://placehold.it/500',
+		},
+		title: {
+			type: 'string',
+			source: 'text',
+			selector: 'h3 > span',
+		},
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: '.text',
+		},
+		link: {
+			source: 'attribute',
+			attribute: 'href',
+			selector: 'a.link',
+		},
+	},
 
 	edit: function( props ) {
-		const { image, title, content } = props;
+		const {
+			attributes: {
+				image,
+				title,
+				content,
+				link,
+			},
+			className,
+			setAttributes,
+		} = props;
+
+		const onChangeContent = content => {
+			setAttributes( { content } );
+		};
+
+		const onChangeTitle = title => {
+			setAttributes( { title } );
+		};
+
+		const onChangeUrl = link => {
+			setAttributes( { link } );
+		};
+
+		const selectImage = ( value ) => {
+			setAttributes( {
+				image: value.sizes.full.url,
+			} );
+		};
+
+		const imageClassNames = [ 'image', '3x4' ].join( ' ' );
+		const titleClassNames = [ 'text-lg', 'lightdark' ].join( ' ' );
+		const contentClassNames = [ 'text' ].join( ' ' );
+
+		{/* <Image src={image} size="3x4"/> */}
+
+		return [
+			<InspectorControls>
+				Options <br />
+				<br />
+
+				<label class="blocks-base-control__label" for="url_id_1">Learn more... URL</label>
+				<RichText
+					id="url_id_1"
+					format="string"
+					onChange={ onChangeUrl }
+					value={ link }
+					placeholder="Enter FULL URL here"
+				/>
+			</InspectorControls>,
+
+			<div className={ className }>
+				<MediaUpload
+					onSelect={ selectImage }
+					render={ ( { open } ) => {
+						return <div
+							className={ imageClassNames }
+							style={ {
+								backgroundImage: `url('${ image }')`,
+							} }
+							onClick={ open }
+						/>;
+					} }
+				/>
+				<h3>
+					<RichText
+						tagName="span"
+						value={ title }
+						onChange={ onChangeTitle }
+						className={ titleClassNames }
+					/>
+				</h3>
+				<div className="card-content">
+					<RichText
+						tagName="p"
+						value={ content }
+						onChange={ onChangeContent }
+						className={ contentClassNames }
+					/>
+				</div>
+				<a className="link" href={ link }>Learn more...</a>
+			</div>,
+		];
+	},
+
+	save: function( props ) {
+		const {
+			attributes: {
+				image,
+				title,
+				content,
+				link,
+			},
+		} = props;
 
 		return (
-			<div className="card">
-				<Image src={ image } size="3x4" />
-				<h3><span className="text-lg lightdark">{ title }</span></h3>
+			<div>
+				<div
+					className="image 3x4"
+					style={ {
+						backgroundImage: `url('${ image }')`,
+					} }
+				/>
+
+				<h3>
+					<span className="text-lg lightdark">{ title }</span>
+				</h3>
+
 				<div className="card-content">
 					<p className="text">{ content }</p>
 				</div>
-				<a className="link" href="#">Learn more...</a>
+
+				<a className="link" href={ link }>Learn more...</a>
 			</div>
 		);
-	},
-
-	save: function( ) {
-		return null;
 	},
 } );
