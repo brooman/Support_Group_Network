24b6c151fb46220371338039971ab981bad84082
André Broman <bromanandre@gmail.com>
2019-04-23 14:57:35 +0200

    Fixed default slug

--- frontend/src/helpers/wordpressAPI.js
+++ frontend/src/helpers/wordpressAPI.js
@@ -1,7 +1,7 @@
 const getPageContent = async (path) => {
 
-  //Fetch pageId with supplied slug or 'index' if none was provided
-  let pageId = await fetch(`http://localhost:8080/wp-json/wp/v2/pages?slug=${path ? path.substr(1) : 'index'}`)
+  //Fetch pageId with supplied slug or 'home' if none was provided
+  let pageId = await fetch(`http://localhost:8080/wp-json/wp/v2/pages?slug=${path.substr(1) ? path.substr(1) : 'home'}`)
     .then(res => res.json())
     .then(data => {
         return data[0].id;
