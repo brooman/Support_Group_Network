90d8dc65208c32e09d64fcfe067acc5742da36a3
Benjamin Fransson <benjamin@cafcon.se>
2019-04-23 22:44:54 +0200

    Implemented title in container

--- wordpress/wp-content/mu-plugins/support-group-network-blocks/src/components/container/index.js
+++ wordpress/wp-content/mu-plugins/support-group-network-blocks/src/components/container/index.js
@@ -1,6 +1,10 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { InnerBlocks } = wp.editor;
+const {
+	RichText,
+	InspectorControls,
+	InnerBlocks,
+} = wp.editor;
 
 registerBlockType( 'cgb/container', {
 
@@ -8,25 +12,73 @@ registerBlockType( 'cgb/container', {
 	icon: 'shield',
 	category: 'sgn-blocks',
 
-	edit: function( props ) {
-		const { title, wide } = props;
+	attributes: {
+		title: {
+			type: 'string',
+			selector: 'span.text-lg',
+			default: '',
+			required: false,
+		},
+	},
 
+	edit: function( props ) {
+		const {
+			attributes: {
+				title,
+			},
+			className,
+			setAttributes,
+		} = props;
+
+		const wide = false;
 		const isWide = wide ? 'wide' : '';
 
-		return (
-			<div className={ `container ${ isWide }` }>
-				{ title &&
+		const onChangeTitle = ( title ) => {
+			setAttributes( { title } );
+		};
+
+		const classNames = [ className, isWide ].join( ' ' );
+
+		return [
+			<InspectorControls>
+				<label className="blocks-base-control__label" htmlFor="name_id_1">Container Title (Optional)</label>
+				<RichText
+					id="name_id_1"
+					format="string"
+					onChange={ onChangeTitle }
+					value={ title }
+					placeholder="Enter a title for the container"
+				/>
+			</InspectorControls>,
+			<div className={ classNames }>
+				{ ( title !== '' ) ? (
 					<h2 className="container-title">
 						<span className="text-lg lightdark">{ title }</span>
-					</h2> }
+					</h2> ) : null }
 				<div className="container-content">
-					<InnerBlocks templateLock={ false } />
+					<InnerBlocks />
+				</div>
+			</div>,
+		];
+	},
+
+	save: function( props ) {
+		const {
+			attributes: {
+				title,
+			},
+		} = props;
+
+		return (
+			<div>
+				{ ( title !== '' ) ? (
+					<h2 className="container-title">
+						<span className="text-lg lightdark">{ title }</span>
+					</h2> ) : null }
+				<div className="container-content">
+					<InnerBlocks.Content />
 				</div>
 			</div>
 		);
-	},
-
-	save: function( ) {
-		return null;
 	},
 }, );
